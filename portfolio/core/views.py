619e013c510de436b6de9398e74a369056f8afe3
pARHA-CS <raphael.mercier-2@etu.univ-tours.fr>
2024-09-19 16:54:33 +0200

    Portfolio fait

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,3 +1,14 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import About, Service, RecentWork, Client
 
-# Create your views here.
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.first()
+        context['works'] = RecentWork.objects.first()
+        return  context
